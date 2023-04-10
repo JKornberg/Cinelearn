9e4b2ba12fff544b30bfa69ea2899614437bfdab
Chaofan Chen <cchen72@syr.edu>
2023-04-10 00:26:47 -0400

    add useful tools

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 A Browser extension that is meant to propagate learning outcomes for individuals looking to learn new languages through watching TV series and Movies.
 
-### Running the server 
+### Installtion
+``` npm install ```
 
-``` npm  start ```
+### Running the server 
+``` npm run start ```
+[http://localhost:3000/](http://localhost:3000/)
+
+### To run tests
+``` npm run test ```
+
+### To run linting
+``` npm run lint ```
+
+###To run linting in watch mode
+``` npm run watch ```
+
+### To run prettier
+``` npm run prettify ```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
+var mysql = require("mysql");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'me',
+//   password : 'secret',
+//   database : 'my_db'
+// });
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+// connection.end();
