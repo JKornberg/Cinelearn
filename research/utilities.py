7423a5678f17f74f007646133b6c3c0840d85bdf
Jonah Kornberg <kornberg.jonah@gmail.com>
2023-04-12 13:32:47 -0400

    add research notebooks

--- research/utilities.py
+++ research/utilities.py
@@ -5,6 +5,56 @@ import pandas as pd
 from lxml import etree
 import numpy as np
 
+def generate_completion(prompt, engine='text-davinci-003',temp=0.7,top_p=1,max_tokens=256,freq_pen=0, presence_pen=0, n=1, stop=None):
+    '''
+        Generic function to generate completion from OpenAI API
+    '''
+    max_retry = 5
+    retry = 0
+    while retry < max_retry:
+        try:
+            response = openai.Completion.create(
+                engine=engine,
+                prompt=prompt,
+                temperature=temp,
+                top_p=top_p,
+                max_tokens=max_tokens,
+                frequency_penalty=freq_pen,
+                presence_penalty=presence_pen,
+                n = n,
+                stop=stop
+            )
+            return [r.text.strip() for r in response.choices]
+        except Exception as e:
+            print(e)
+            retry += 1
+            print("retrying")
+            
+def generate_completion_chat(messages, model='gpt-4',temp=0.7,top_p=1,max_tokens=256,freq_pen=0, presence_pen=0, n=1, stop=None):
+    '''
+        Generic function to generate completion from OpenAI API
+    '''
+    max_retry = 5
+    retry = 0
+    while retry < max_retry:
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temp,
+                top_p=top_p,
+                max_tokens=max_tokens,
+                frequency_penalty=freq_pen,
+                presence_penalty=presence_pen,
+                n = n,
+                stop=stop
+            )
+            return [r['message']['content'] for r in response['choices']]
+        except Exception as e:
+            print(e)
+            retry += 1
+            print("retrying")
+
 def transcript_to_df(path, merge_time=True):
     # If merge_time is True, then combine same time segments into a single string
     # Parse the XML content
@@ -25,7 +75,6 @@ def transcript_to_df(path, merge_time=True):
             "end": p.attrib["end"][:-1],
             "text": text
         }
-        print(row)
         data.append(row)
     data = pd.DataFrame(data)
     data = data[~data.isnull().any(axis=1)]
@@ -37,8 +86,11 @@ def transcript_to_df(path, merge_time=True):
 
     return data
 
-def df_to_text(df):
-    return df["text"].str.cat(sep="\n")
+def df_to_text(df, index=False):
+    if index:
+        return '\n'.join([f'{i+1}. {row["text"]}' for i, row in df.iterrows()])
+    else:
+        return '\n'.join([f'{row["text"]}\n' for i, row in df.iterrows()])
 
 
 
