8a603780bd962db751efded970c95d65109cf47b
knatara1 <karthik1.n2@gmail.com>
2023-05-03 04:45:47 -0400

    Finished all of the API calls

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ const connection = mysql.createConnection({
   port: 3306,
   user: 'admin',
   password: '***REMOVED***',
-  database: 'cinelearn'
+  database: 'cinelearn',
+  multipleStatements: true
 });
 
 connection.connect()
@@ -23,12 +24,9 @@ router.get("/hello", function (req, res, next) {
   res.send("Hello World");
 });
 
-router.get('/getEpisode', (req, res) => {
+router.get('/getSpanishSubs', (req, res) => {
   episode_num = req.query.episode_num
-  user_id = req.query.user_id
-  spanish_subtitle_data = []
-  english_subtitle_data = []
-  connection.query(`SELECT * FROM subtitle WHERE episode_id=${episode_num} AND english_subtitle=\'\' ORDER BY start_time`, (err, rows, fields) => 
+  connection.query(`SELECT * FROM subtitle WHERE episode_id=${episode_num} AND english_subtitle=\'\' ORDER BY start_time DESC`, (err, rows, fields) => 
     {
       if(err) throw err
       spanish_subtitle_data = rows
@@ -41,7 +39,7 @@ router.get('/getEpisode', (req, res) => {
 
 router.get('/getEnglishSubs', (req, res) => {
   episode_num = req.query.episode_num
-  connection.query(`SELECT * FROM subtitle WHERE episode_id=${episode_num} AND spanish_subtitle=\'\' ORDER BY start_time`, (err, rows, fields) => 
+  connection.query(`SELECT * FROM subtitle WHERE episode_id=${episode_num} AND spanish_subtitle=\'\' ORDER BY start_time DESC`, (err, rows, fields) => 
     {
       if(err) throw err
       res.status(200).json({
@@ -92,7 +90,7 @@ router.get('/getVocabQuestions', (req, res) => {
     })
 })
 
-router.get('/getEpisodeContextProgress', (req, res) => {
+router.get('/getUserContextProgress', (req, res) => {
   episode_num = req.query.episode_num
   user_id = req.query.user_id
   connection.query(`SELECT user_id,SUM(correct),episode_id 
@@ -100,37 +98,178 @@ router.get('/getEpisodeContextProgress', (req, res) => {
   JOIN vocab on vocab.vocab_id=vocab_progress.vocab_id GROUP BY user_id HAVING user_id=${user_id} AND episode_id=${episode_num}`, 
   (err, rows, fields) => 
     {
-      question_list = []
-      rows = rows
+      user_correct_num = rows[0]['SUM(correct)']
+      episode_id = rows[0]['episode_id']
+      user_id = rows[0]['user_id']
       if(err) throw err
       res.status(200).json({
         status: "success",
-        data: rows,
-      });
-    })
-})
-
-router.get('/getEpisodeVocabProgress', (req, res) => {
-  episode_num = req.query.episode_num
-  user_id = req.query.user_id
-  connection.query(`SELECT user_id,SUM(correct),episode_id 
-  FROM progress JOIN question_progress ON progress.question_progress_id=question_progress.progress_id 
-  JOIN question on question.question_id=question_progress.question_id GROUP BY user_id HAVING user_id=${user_id} AND episode_id=${episode_num}`, 
-  (err, rows, fields) => 
-    {
-      rows = rows[0]['SUM(correct)']
-      console.log(rows)
-      if(err) throw err
-      res.status(200).json({
-        status: "success",
-        correct_num: rows,
+        user_correct: user_correct_num,
+        episode_id: episode_id,
         user_id: user_id
       });
     })
 })
 
+router.get('/getUserVocabProgress', (req, res) => {
+  episode_num = req.query.episode_num
+  user_id = req.query.user_id
+  connection.query(`SELECT user_id,SUM(correct),episode_id 
+  FROM progress JOIN vocab_progress ON progress.vocab_progress_id=vocab_progress.progress_id 
+  JOIN vocab on vocab.vocab_id=vocab_progress.vocab_id GROUP BY user_id HAVING user_id=${user_id} AND episode_id=${episode_num}`, 
+  (err, rows, fields) => 
+    {
+      user_correct_num = rows[0]['SUM(correct)']
+      episode_id = rows[0]['episode_id']
+      user_id = rows[0]['user_id']
+      if(err) throw err
+      res.status(200).json({
+        status: "success",
+        user_correct: user_correct_num,
+        episode_id: episode_id,
+        user_id: user_id
+      });
+    })
+})
 
+router.get('/getEpisodeContextInfo', (req, res) => {
+  episode_num = req.query.episode_num
+  user_id = req.query.user_id
+  connection.query(`SELECT user_id,language,english_question,spanish_question,difficulty,correct,episode_id FROM question_progress 
+  JOIN progress ON progress.question_progress_id=question_progress.progress_id JOIN question 
+  ON question.question_id=question_progress.question_id WHERE user_id=${user_id} AND episode_id=${episode_num}`, 
+  (err, rows, fields) => 
+    {
+      context_question_list = []
+      for(var i = 0; i < rows.length; i++) {
+        context_question_dict = {}
+        context_question_dict.language = rows[i]['language']
+        context_question_dict.english_question = rows[i]['english_question']
+        context_question_dict.spanish_question = rows[i]['spanish_question']
+        context_question_dict.difficulty = rows[i]['difficulty']
+        context_question_dict.correct = rows[i]['correct']
+        context_question_dict.episode_id = rows[i]['episode_id']
+        context_question_dict.user_id = rows[i]['user_id']
+        context_question_list.push(context_question_dict)
+      }
+      if(err) throw err
+      res.status(200).json({
+        status: "success",
+        context_question_list: context_question_list
+      });
+    })
+})
 
+router.get('/getEpisodeVocabInfo', (req, res) => {
+  episode_num = req.query.episode_num
+  user_id = req.query.user_id
+  connection.query(`SELECT user_id,language,english_vocab,spanish_vocab,difficulty,correct,episode_id,explanation FROM vocab_progress 
+  JOIN progress ON progress.vocab_progress_id=vocab_progress.progress_id JOIN vocab 
+  ON vocab.vocab_id=vocab_progress.vocab_id WHERE user_id=${user_id} AND episode_id=${episode_num}`, 
+  (err, rows, fields) => 
+    {
+      if(err) throw err
+      res.status(200).json({
+        status: "success",
+        vocab_question_list: rows
+      });
+    })
+})
+
+function hashcode(s) {
+  var h = 0, l = s.length, i = 0;
+  if ( l > 0 )
+    while (i < l)
+      h = (h << 5) - h + s.charCodeAt(i++) | 0;
+  return h;
+};
+
+router.get('/authUser', (req, res) => {
+  username = req.query.username
+  password = req.query.hashed_password
+  connection.query(`SELECT password FROM user where user_name='${username}'`, 
+  (err, rows, fields) => 
+    {
+      if(err) throw err
+      if(password == rows[0].password) {
+        res.status(200).json({
+          status: "success",
+        });
+      }
+      else {
+        res.status(200).json({
+          status: "failed",
+        });
+      }
+    })
+})
+
+router.get('/createUser', (req, res) => {
+  username = req.query.username
+  password = req.query.hashed_password
+  connection.query(`INSERT INTO user (user_name, password) VALUES ('${username}', '${password}');`, 
+  (err, rows, fields) => 
+    {
+      if(err) throw err
+      else {
+        res.status(200).json({
+          status: "success",
+        });
+      }
+    })
+})
+
+router.get('/createContextAnswer', (req, res) => {
+  question_id = req.query.question_id
+  correct = req.query.correct
+  time = req.query.time
+  language = req.query.language
+  user_id = req.query.user_id
+  connection.query(`INSERT INTO question_progress (question_id, correct,time)
+  VALUES (${question_id},${correct},${time}); SELECT progress_id FROM question_progress WHERE progress_id=LAST_INSERT_ID()`, 
+  (err, rows, fields) => 
+    {
+      if(err) throw err
+      else {
+        progress_id = JSON.parse(JSON.stringify(rows[1]))[0].progress_id
+        connection.query(`INSERT INTO progress (user_id, language,question_progress_id)
+        VALUES (${user_id},${language},${progress_id})`), 
+        (err, rows, fields) => 
+          {
+            if(err) throw err
+          }  
+          res.status(200).json({
+            status: "success",
+          });
+        }
+    })
+})
+
+router.get('/createVocabAnswer', (req, res) => {
+  question_id = req.query.question_id
+  correct = req.query.correct
+  time = req.query.time
+  language = req.query.language
+  user_id = req.query.user_id
+  connection.query(`INSERT INTO vocab_progress (vocab_id, correct,time)
+  VALUES (${question_id},${correct},${time}); SELECT progress_id FROM vocab_progress WHERE progress_id=LAST_INSERT_ID()`, 
+  (err, rows, fields) => 
+    {
+      if(err) throw err
+      else {
+        progress_id = JSON.parse(JSON.stringify(rows[1]))[0].progress_id
+        connection.query(`INSERT INTO progress (user_id, language,vocab_progress_id)
+        VALUES (${user_id},${language},${progress_id})`), 
+        (err, rows, fields) => 
+          {
+            if(err) throw err
+          }  
+          res.status(200).json({
+            status: "success",
+          });
+        }
+    })
+})
 
 const LocalStrategy = require('passport-local').Strategy;
 
