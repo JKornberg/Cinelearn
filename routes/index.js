5b869b1f3dd9364bc6b1566b7702622f3a8cafae
Jonah Kornberg <kornberg.jonah@gmail.com>
2023-05-15 22:18:37 -0400

    push to new origin

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ var mixpanel = Mixpanel.init('ececd3d662d1259408c9b162565367ef');
 // var privateKey = fs.readFileSync('./private-key.pem', 'utf8');
 /* GET home page. */ 
 const connection = mysql.createConnection({
-  host: '***REMOVED***',
+  host: 'database-1.cvifg4azbmac.us-east-2.rds.amazonaws.com',
   port: 3306,
   user: 'admin',
-  password: '***REMOVED***',
+  password: 'Cinelearn!',
   database: 'cinelearn',
   multipleStatements: true
 });
